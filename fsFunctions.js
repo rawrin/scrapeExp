c51688a47d263df8d5fb8a439f4b2c09b9f95bc9
gregorylull <gregorylull@gmail.com>
2014-04-21 09:00:53 -0700

    fsFunctions work. has executeTest commented out

--- fsFunctions.js
+++ fsFunctions.js
@@ -13,14 +13,19 @@ var saveAsJson = function (item, dir) {
           .replace()
           .toLowerCase();
           //if then for directory
-          if (archive.indexOf(item.file + '.json') === -1 && main.indexOf(item.file + '.json') === -1){
-            scrapeSite(item.link, item.title, item);
-          }
+  // if (archive.indexOf(item.file + '.json') === -1 && main.indexOf(item.file + '.json') === -1){
+  //   scrapeSite(item.link, item.title, item);
+  // }
 
   return fs.writeFileAsync(path.join(dir, item.file + '.json'), JSON.stringify(item));
 };
 
+var executeTest = function () {
+  t1 = {title: "hello", link: 'www.greg.com', wordunique: 3, wordtable: {'i' : 1, 'like': 1, 'dogs': 1}};
+  t2 = {title: "hello something esle's aosk agood HPAPY!", link: 'www.asldb.com', wordunique: 3, wordtable: {'i' : 1, 'like': 1, 'dogs': 1}};
 
-t1 = {title: "hello", link: 'www.greg.com', wordunique: 3, wordtable: {'i' : 1, 'like': 1, 'dogs': 1}};
+  saveAsJson(t1);
+  saveAsJson(t2);
+};
 
-saveAsJson(t1);
+executeTest();
