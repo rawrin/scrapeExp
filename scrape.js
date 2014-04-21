4821edaf150750b8ce03ae3e5dda51ff0c2f2f1c
gregorylull <gregorylull@gmail.com>
2014-04-21 07:29:57 -0700

    created a Queue class (functional instantiation) for keeping track of what to scrape next so readability does not overload

--- scrape.js
+++ scrape.js
@@ -1,3 +1,19 @@
+/*
+
+  LOGIC: 
+
+  If readability is sucessful
+    - update mongo
+      if successful
+        update mongo master
+        remove from queue
+        update queue master?
+
+  Add to scrape queue IF
+    not in scrapeQueue AND not in mongoMaster
+*/
+
+
 var common = { the:null,be:null,to:null,of:null,and:null,a:null,in:null,that:null,have:null,I:null,it:null,for:null,not:null,on:null,with:null,he:null,as:null,you:null,do:null,at:null,Word:null,this:null,but:null,his:null,by:null,from:null,they:null,we:null,say:null,her:null,she:null,or:null,an:null,will:null,my:null,one:null,all:null,would:null,there:null,their:null,what:null,so:null,up:null,out:null,if:null,about:null,who:null,get:null,which:null,go:null,me:null,when:null,make:null,can:null,like:null,time:null,no:null,just:null,him:null,know:null,take:null,people:null,into:null,year:null,your:null,good:null,some:null,could:null,them:null,see:null,other:null,than:null,then:null,now:null,look:null,only:null,come:null,its:null,over:null,think:null,also:null,back:null,after:null,use:null,two:null,how:null,our:null,work:null,first:null,well:null,way:null,even:null,new:null,want:null,because:null };
 var FeedParser = require('feedparser');
 var request = require('request');
@@ -6,11 +22,25 @@ var Sanitizer = require('sanitizer');
 var cheerio = require('cheerio');
 var CronJob = require('cron').CronJob;
 
+var rssQ = require('./rssQueue.js');
 
-masterRssList = {};
 var rssURL = "https://news.ycombinator.com/rss";
 
-// readability cron job
+// master lists
+var masterRssList = {};
+var scrapeQueue = rssQ.makeScrapeQueue();
+
+/***
+ *       _____                    
+ *      / ____|                   
+ *     | |      _ __  ___   _ __  
+ *     | |     | '__|/ _ \ | '_ \ 
+ *     | |____ | |  | (_) || | | |
+ *      \_____||_|   \___/ |_| |_|
+ *                                
+ *                                
+ */
+
 var readabilityRequestCron = function (time, master) {
   master = master || masterRssList;
   var time = time || '*/5 * * * * *';
@@ -18,13 +48,86 @@ var readabilityRequestCron = function (time, master) {
   console.log('You will see this message every 5 sec');
 
   // 
-  populateMasterRss();
+  populateMasterRssQueue();
 
   // 
   queryReadability();
 
   }, null, true, "America/Los_Angeles");
 };
+
+// populates rss master list upon successful rss read
+var populateMasterRssQueue = function (url) {
+  url = url || rssURL;
+
+  rssReader(url).then(function(rssResults){
+
+    console.log('rss scrape results: ', toRssResultTitles(rssResults));
+
+    var addedNew = false;
+
+    // check each item from the rss result and add to the queue IF
+    //   1. it's not contained in scrapeQueue
+    //   2. it's not in the mongoDB TODO (add inside the if statement)
+    for (var i = 0; i < rssResults.length; i++) {
+      var rssObj = rssResults[i];
+      if (isRssDocValid(rssObj) && !scrapeQueue.contains(rssObj)) {
+        var add = scrapeQueue.queue(rssObj);
+        console.log('added to scrapeQueue: ', add);
+        addedNew = true;
+      }
+    }
+
+    if (addedNew) {
+      console.log('\ncurrent rss list: \n', scrapeQueue.all());
+    }
+    // // if a rss doc is not in the masterRss list, then add it
+    // for (var i = 0; i < rssResults.length; i++) {
+    //   var item = rssResults[i];
+    //   if (!isInRssMaster(item)) {
+    //     addToMaster(item);
+    //     console.log('added to rss master: ', item.title);
+    //     addedNew = true;
+    //   }
+    // }
+  })
+  .catch(function(err){
+    console.log('rssReader did not read rss: ', err);
+  });
+};
+
+// calls readability function which queries the readability api and returns a parsed object to be passed to wordTableMaker
+//   return wordTableMaker(doc);
+
+var queryReadability = function (doc, master) {
+  master = master || masterRssList;
+  var masterLength
+  if (master.length > 0) {
+    var doc = master[master.length-1];
+
+    console.log('\n readability doc: \n', doc.title, ' ', doc.link);
+
+    readableQuery(doc.link)
+    .then(function (doc) {
+      console.log('readableQuery worked: ', doc.title);
+      master.pop();
+    })
+    .catch(function(err){
+      console.log('cron rss query did not work');
+    });
+  }
+};
+
+/***
+ *      _    _        _                    
+ *     | |  | |      | |                   
+ *     | |__| |  ___ | | _ __    ___  _ __ 
+ *     |  __  | / _ \| || '_ \  / _ \| '__|
+ *     | |  | ||  __/| || |_) ||  __/| |   
+ *     |_|  |_| \___||_|| .__/  \___||_|   
+ *                      | |                
+ *                      |_|                
+ */
 
 // converts rss query results to an array of rss titles
 var toRssResultTitles = function (master) {
@@ -50,7 +153,7 @@ var isInRssMaster = function (doc, master) {
 };
 
 var isRssDocValid = function (doc) {
-  if (!doc) { throw "no doc inserted"}
+  if (!doc) { console.log ('rss doc not valid!!!', doc)}
   if (typeof doc.title !== 'string' && typeof doc.link !== 'string') {
     return false;
   } 
@@ -74,7 +177,7 @@ var addToMaster = function (doc) {
 };
 
 // returns an array of titles from the master rss list
-var currentMasterRss = function (master) {
+var currentMasterRssQueue = function (master) {
   master = master || masterRssList;
   var results = [];
   for (var item in master) {
@@ -83,58 +186,20 @@ var currentMasterRss = function (master) {
   return results;
 };
 
-// populates rss master list upon successful rss read
-var populateMasterRss = function () {
-
-  rssReader(rssURL).then(function(rssResults){
-
-    console.log('rss scrape results: ', toRssResultTitles(rssResults));
-
-    var addedNew = false;
-
-    // if a rss doc is not in the masterRss list, then add it
-    for (var i = 0; i < rssResults.length; i++) {
-      var item = rssResults[i];
-      if (!isInRssMaster(item)) {
-        addToMaster(item);
-        console.log('added to rss master: ', item.title);
-        addedNew = true;
-      }
-    }
-    if (addedNew) {
-      console.log('\ncurrent rss list: \n', currentMasterRss());
-    }
-  })
-  .catch(function(err){
-    console.log('rssReader did not read rss: ', err);
-  });
-};
 
 
-// calls readability function which queries the readability api and returns a parsed object to be passed to wordTableMaker
-//   return wordTableMaker(doc);
+/***
+ *       ____                      _            
+ *      / __ \                    (_)           
+ *     | |  | | _   _   ___  _ __  _   ___  ___ 
+ *     | |  | || | | | / _ \| '__|| | / _ \/ __|
+ *     | |__| || |_| ||  __/| |   | ||  __/\__ \
+ *      \___\_\ \__,_| \___||_|   |_| \___||___/
+ *                                              
+ *                                              
+ */
 
-var queryReadability = function (doc, master) {
-  master = master || masterRssList;
-  var masterLength
-  if (master.length > 0) {
-    var doc = master[master.length-1];
-
-    console.log('\n readability doc: \n', doc.title, ' ', doc.link);
-
-    readableQuery(doc.link)
-    .then(function (doc) {
-      console.log('readableQuery worked: ', doc.title);
-      master.pop();
-    })
-    .catch(function(err){
-      console.log('cron rss query did not work');
-    });
-  }
-};
-
-// helper functions
-
+// Makes a request to rss url and returns a promised array of rss objects
 var rssReader = function(url) {
   return new Promise(function(resolve, reject){
     var req = request(url);
@@ -226,6 +291,30 @@ var wordTableMaker = function(doc) {
   });
 };
 
+/***
+ *      ______                            _   
+ *     |  ____|                          | |  
+ *     | |__   __  __ _ __    ___   _ __ | |_ 
+ *     |  __|  \ \/ /| '_ \  / _ \ | '__|| __|
+ *     | |____  >  < | |_) || (_) || |   | |_ 
+ *     |______|/_/\_\| .__/  \___/ |_|    \__|
+ *                   | |                      
+ *                   |_|                      
+ */
+
+module.exports.readabilityRequestCron = readabilityRequestCron;
+
+
+/***
+ *       _____              _         _   
+ *      / ____|            (_)       | |  
+ *     | (___    ___  _ __  _  _ __  | |_ 
+ *      \___ \  / __|| '__|| || '_ \ | __|
+ *      ____) || (__ | |   | || |_) || |_ 
+ *     |_____/  \___||_|   |_|| .__/  \__|
+ *                            | |         
+ *                            |_|         
+ */
 readabilityRequestCron();
 
 // rssReader("https://news.ycombinator.com/rss").then(function(rss){console.log(rss);});
